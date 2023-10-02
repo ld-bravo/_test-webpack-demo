9bfdc00a751a94828ef157d134b96fdac9635e44
Lester D. Bravo <dago@outlook.com>
2023-10-02 16:01:55 -0600

    Fixed bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js'
+  },
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
@@ -41,8 +43,5 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
-  },
-  optimization: {
-    runtimeChunk: 'single'
   }
 };
