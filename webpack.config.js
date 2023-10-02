05ed4261d54168e8e5d7a5a1c7158b36f09c353c
Lester D. Bravo <dago@outlook.com>
2023-10-02 14:54:11 -0600

    First commit.

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  module: {
+    rules: [
+      // Include CSS on js file
+      {
+        // test: /\.css$/i,
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates 'style' nodes from JS strings
+          'style-loader',
+
+          // Translates CSS into CommonJS
+          'css-loader',
+
+          // Compiles Sass to CSS
+          "sass-loader"
+        ],
+      },
+      // Include images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  }
+};
